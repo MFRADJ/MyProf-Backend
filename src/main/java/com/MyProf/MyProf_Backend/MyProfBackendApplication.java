1c8e3454cb295679a0ad442c6c9c07ce14c5f459
MFRADJ <mohamed-fradj@outlook.com>
2024-12-30 19:17:38 +0100

    Initialisation du branche main "first commit"

---
+++ src/main/java/com/MyProf/MyProf_Backend/MyProfBackendApplication.java
@@ -0,0 +1,13 @@
+package com.MyProf.MyProf_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProfBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProfBackendApplication.class, args);
+	}
+
+}
