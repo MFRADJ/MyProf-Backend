b02c3aa4ff5cb3082eee744bf97067fbd231942a
MFRADJ <mohamed-fradj@outlook.com>
2025-01-01 23:24:38 +0100

    tests workflows_qualityCheck _4

--- src/main/java/com/MyProf/MyProf_Backend/MyProfBackendApplication.java
+++ src/main/java/com/MyProf/MyProf_Backend/MyProfBackendApplication.java
@@ -8,12 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class MyProfBackendApplication {
+    
+    /**
+     * Private constructor to hide the implicit public one.
+     */
+    private MyProfBackendApplication() {
+    }
 
     /**
      * Main method to start the application.
      * @param args command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(MyProfBackendApplication.class, args);
     }
 
